823099ebee63d206bfe813bc2e518235840217f7
Alexandria Davis <alexandriadavis@csumb.edu>
2019-02-22 12:46:12 -0800

    Add more sample data

--- test-dashboard/src/app/test-list/test-list.component.ts
+++ test-dashboard/src/app/test-list/test-list.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-list.component.scss']
 })
 export class TestListComponent implements OnInit {
-
+  tests;
   constructor() { }
 
   ngOnInit() {
+    this.tests = [
+      {
+        test_name:"sample_test_1",
+        durration:"3M",
+        status:"Pass",
+        historical_pass_rate:"50%",
+        historical_run_rate:"100%"
+      },
+    ]
   }
-
 }
