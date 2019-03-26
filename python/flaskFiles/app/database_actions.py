63c7b59f71512c84e60cf4c23a5b005a379d070e
Alexandria Davis <alexandriadavis@csumb.edu>
2019-03-25 21:15:52 -0700

    Fix problem with seeding (Seeding can now only happen once)

--- python/flaskFiles/app/database_actions.py
+++ python/flaskFiles/app/database_actions.py
@@ -25,12 +25,13 @@ class database_actions:
 
     def seed():
         #projects
-        project1 = projects(id=None,project_name="testproject1")
+        project1 = projects(id=1,project_name="testproject1")
         db.session.add(project1)
         project2 = projects(id=None,project_name="testproject2")
         db.session.add(project2)
+        db.session.commit()
         #test runs
-        testrun1 = testRun(id=None,name="testrun1",project=1,date=None)
+        testrun1 = testRun(id=1,name="testrun1",project=1,date=None)
         db.session.add(testrun1)
         testrun2 = testRun(id=None,name="testrun2",project=1,date=None)
         db.session.add(testrun2)
@@ -38,8 +39,9 @@ class database_actions:
         db.session.add(testrun3)
         testrun4 = testRun(id=None,name="testrun4",project=2,date=None)
         db.session.add(testrun4)
+        db.session.commit()
         #test suites
-        testsuite1 = test_suite(id=None,testsuite="testsuite1", project=1,runtime=1.1)
+        testsuite1 = test_suite(id=1,testsuite="testsuite1", project=1,runtime=1.1)
         db.session.add(testsuite1)
         testsuite2 = test_suite(id=None,testsuite="testsuite2", project=2,runtime=7.7)
         db.session.add(testsuite2)
@@ -47,18 +49,20 @@ class database_actions:
         db.session.add(testsuite3)
         testsuite4 = test_suite(id=None,testsuite="testsuite4", project=1,runtime=4.35)
         db.session.add(testsuite4)
+        db.session.commit()
         #test names
-        test1 = test_names(id=None,test_name="test1",project=1)
+        test1 = test_names(id=1,test_name="test1",project=1)
         db.session.add(test1)
         test2 = test_names(id=None,test_name="test1",project=1)
         db.session.add(test2)
         test3 = test_names(id=None,test_name="test1",project=1)
         db.session.add(test3)
+        db.session.commit()
         #test_case
-        testcase1 = test_case(id=None, test_id=1, test_suite=1,classname="idk",time=12.2,status="failed",launched=None)
+        testcase1 = test_case(id=1, test_id=1, test_suite=1,classname="idk",time=12.2,status="failed",launched=None)
         db.session.add(testcase1)
+        db.session.commit()
         #issues
         issue1 = issues(id = 1, test=1, output="Test output. In actuallity this will get very long and very messy. I'm just leaving this here though", status="fail")
-
         db.session.commit()
         return {"seeded":"true"}
