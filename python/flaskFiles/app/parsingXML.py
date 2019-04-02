230894aedda633c6216a8d5a205e6790c141e28a
Irais1 <iraisgoparosorio@Iraiss-MacBook-Pro.local>
2019-04-02 16:45:20 -0700

    Date and CDATA parsed

--- python/flaskFiles/app/parsingXML.py
+++ python/flaskFiles/app/parsingXML.py
@@ -2,28 +2,36 @@
 from xml.dom import minidom
 import pprint
 import xml.etree.ElementTree as etree
-import xml.etree.ElementTree as ElementTree
 
 def parsexmlFile(testFileName):
     myDoc = minidom.parse(testFileName)
-    
+
     testSuite = myDoc.getElementsByTagName('testsuite')
     testCases = myDoc.getElementsByTagName('testcase')
-    
+    properties = myDoc.getElementsByTagName('property')
+
     totalTests = testSuite[0].attributes['tests'].value
     numErrors = testSuite[0].attributes['errors'].value
     failedTest = testSuite[0].attributes['failures'].value
     numSkipped = testSuite[0].attributes['skipped'].value
-    
+
     name = testSuite[0].attributes['name'].value
     totalDuration = testSuite[0].attributes['time'].value
+
     overallInfo= {}
     suiteInfo = []
     indivTestInfo = []
     testInfo = []
-    
+
+    for property in properties:
+        name = property.attributes['name'].value
+        if(name == "sun.java.command"):
+            value = property.attributes['value'].value
+            split = value.split()
+            date = split[2][0:10]
+
     for elem in testCases:
-        
+
         time = elem.attributes['time'].value
         error = elem.getElementsByTagName('error')
         failure = elem.getElementsByTagName('failure')
@@ -35,28 +43,20 @@ def parsexmlFile(testFileName):
         errorMessage = 'None'
         failureMessage = 'None'
         skippedMessage = 'None'
-        
+
         if(error != [] and time != 0):
             errorBool = True
-            errorMessage = 'Error'
-            #            root = etree.Element(elem.toString())
-            #            for log in root.xpath(".//error"):
-            #                print log.text
-            #            print(error)
-#            tree = etree.parse(testFileName)
-#            for elem in tree.findall(".//error"):
-#                print elem.findtext("error")
+            root = error[0].firstChild
+            errorMessage = root.data
         elif(failure != [] and time != 0):
             failureBool = True
-            failureMessage = failure[0].attributes['message'].value
-        #            file = etree.parse(testFileName)
-        #            root = file.findall('./testcase/failure').Element('![CDATA[')
-        #            print(root)
+            root = failure[0].firstChild
+            failureMessage = root.data
         elif(time == '0'):
             skippedBool = True
             skippedMessage = skipped[0].attributes['message'].value
         indivTestInfo.append({'time':time,'error': errorBool,'errorMessage': errorMessage,'failure': failureBool,'failureMessage': failureMessage,'ignored': skippedBool,'ignoredMessage': skippedMessage,'testName':elem.attributes['name'].value,'className':className})
-    suiteInfo.append({'suiteName':name,'totalTime':totalDuration,'totalTest':totalTests,'numErrors':numErrors,'failedTest':failedTest,'numSkipped':numSkipped})
+    suiteInfo.append({'suiteName':name,'totalTime':totalDuration,'totalTest':totalTests,'numErrors':numErrors,'failedTest':failedTest,'numSkipped':numSkipped, 'date':date})
 
     overallInfo.update({'SuiteInfo': suiteInfo})
     overallInfo.update({'Info': indivTestInfo})
