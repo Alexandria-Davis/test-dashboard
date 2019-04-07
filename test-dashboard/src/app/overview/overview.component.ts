d395f3de8e6e437809a9d13a475a9b5a59ae349f
Cameron Morefield <cmorefield@csumb.edu>
2019-04-08 04:40:58 +0800

    test cleanup

--- test-dashboard/src/app/overview/overview.component.ts
+++ test-dashboard/src/app/overview/overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 /*import { FailuresComponent } from './failures/index';*/
+import {TestsService} from '../tests.service';
 
 @Component({
   selector: 'project-overview',
@@ -15,17 +16,23 @@ export class OverviewComponent implements OnInit {
   new_fail;
   no_run;
 
-  constructor() { }
+  constructor(private testService: TestsService) { }
 
   ngOnInit() {
-    /*Start Temp Data*/
-    this.passed = 3;
-    this.failed = 6;
-    this.no_run = 2;
+    // /*Start Temp Data*/
+    // this.passed = 3;
+    // this.failed = 6;
+    // this.no_run = 2;
+    // this.total = this.passed + this.failed + this.no_run;
+    // this.new_fail = 4;
+    // this.durration = "34H 23M 11S";
+    // /*End Temp Data*/
+    this.passed = this.testService.getOverallSuccess();
+    this.failed = this.testService.getOverallFailure();
+    this.no_run = this.testService.getOverallDNR();
     this.total = this.passed + this.failed + this.no_run;
     this.new_fail = 4;
-    this.durration = "34H 23M 11S";
-    /*End Temp Data*/
+    this.durration = this.testService.getRuntime();
   }
 
 
--- test-dashboard/src/app/tests.service.ts
+++ test-dashboard/src/app/tests.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,30 @@ import { Injectable } from '@angular/core';
 })
 export class TestsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTestManifest(){
+    return this.http.get('https://0.0.0.0/tests')
+  }
+
+  getTest(testId): any {
+    return this.http.get('https://0.0.0.0/testId')
+  }
+  
+  getOverallSuccess(){
+    return this.http.get<any>('https://0.0.0.0/success')
+
+  }
+  getOverallFailure(){
+    return this.http.get<any>('https://0.0.0.0/failure')
+
+  }
+  getOverallDNR(){
+    return this.http.get<any>('https://0.0.0.0/DNR')
+
+  }
+  getRuntime(){
+    return this.http.get<any>('https://0.0.0.0/runtime')
+
+  }
 }
