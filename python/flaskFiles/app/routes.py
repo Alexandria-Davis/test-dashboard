d0fcd346bcf55b0b936d46c38761fc86ad6e18f0
Alexandria Davis <alexandriadavis@csumb.edu>
2019-05-08 20:08:05 -0700

    actually a stash

--- python/flaskFiles/app/routes.py
+++ python/flaskFiles/app/routes.py
@@ -10,16 +10,47 @@ from app.database_actions import database_actions
 from app.parsingXML import parsexmlFile
 from pprint import pprint
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 @app.route('/index')
 def index():
-    xmldir = 'app/xml/'
-    testFileName = 'TEST-SmokeTest.xml'
-
-    testInfo = parsexmlFile(xmldir + testFileName)
-    pprint(testInfo)
-    database_actions.add_from_file(testInfo)
-    return render_template('index.html', title = testFileName, TestInformation = testInfo)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            testInfo = parsexmlFile(xmldir + testFileName)
+            # pprint(testInfo)
+            database_actions.add_from_file(testInfo)
+            return render_template('index.html', title = testFileName, TestInformation = testInfo)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+    # xmldir = 'app/xml/'
+    # testFileName = 'TEST-SmokeTest.xml'
+    #
+    # testInfo = parsexmlFile(xmldir + testFileName)
+    # pprint(testInfo)
+    # database_actions.add_from_file(testInfo)
+    # return render_template('index.html', title = testFileName, TestInformation = testInfo)
 
 @app.route('/api')
 def api():
